7af5a2b222ec4e852d6735eb290aaea823fe35fe
Suhas Patnaik <suhaspatnaik111@gmail.com>
2025-03-10 23:55:14 +0530

    Created a navbar for the products + Imported products data via data/index.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Controls from "./components/Controls";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [frameZoom, setFrameZoom] = useState(false);
@@ -15,6 +16,7 @@ function App() {
           frameZoom && "min-w-[97vw] min-h-[97vh]"
         } w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-xl resize overflow-auto transition-all duration-100 relative`}
       >
+        <Navbar />
         <Controls onZoom={handleZoom} frameZoom={frameZoom} />
       </div>
     </div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <div className="2xl:w-80 xl:w-52 w-44 h-full bg-gray-200 flex flex-col justify-between pt-5 pl-6 pb-14 md:pb-0">
+      <a
+        href="#"
+        className="2xl:text-2xl xl:text-xl font-light text-red-600 tracking-wider mb-14"
+      >
+        Apple Products
+      </a>
+    </div>
+  );
+}
